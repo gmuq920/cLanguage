582987368cd450759652f736814a24e22c9dab76
gmuq920 <128571797+gmuq920@users.noreply.github.com>
2025-05-07 09:38:54 +0800

    Add files via upload

---
+++ Project_5_5/Project_5_5/test.c
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//用for loop打印10以内数字
+//int main() 
+//{
+//	for (int i = 1; i <= 10; i++) 
+//	{
+//		printf("%d ", i);
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+//计算1-100之间3的倍数的数字之和
+
+//int main()
+//{
+//	int sum = 0;
+//	for (int i = i; i <= 100; i++)
+//	{
+//		if (0 == i % 3)
+//		{
+//			sum += i;
+//		}
+//	}
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
+
+////使用do while打印1-10
+//int main()
+//{
+//	int i = 1;
+//	do {
+//		printf("%d ", i);
+//		i++;
+//	}
+//
+//	while (i <= 10);
+//
+//	return 0;
+//}
+
+////输入一个正整数计算几位，使用do while
+//int main() 
+//{
+//	int i = 9;
+//	int count = 0;
+//
+//	do {
+//		count++;
+//		i /= 10;
+//	} while (i);//Stop when i return to 0
+//
+//	printf("%d ", count);
+//	return 0;
+//
+//}
+
+
+
+
+////输出100-200之间的素数
+//int main() 
+//{
+//	for (int i = 100; i <= 200; i++) 
+//	{
+//		int bool = 1;
+//		for (int numTest = 2; numTest < i; numTest++) 
+//		{
+//			if (0 == i % numTest)
+//				bool = 0;
+//		}
+//		if (bool)
+//		{
+//			printf("%d ", i);
+//		}		
+//	}
+//	return 0;
+//
+//}
