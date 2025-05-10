49a2b8e692b5398e090b4b79b42151a7966e6042
gmuq920 <128571797+gmuq920@users.noreply.github.com>
2025-05-10 08:31:30 +0800

    Add files via upload

---
+++ Project_5_9/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+//
+//#include <stdio.h>
+//int ADD(int x, int y) 
+//{
+//	return x + y;
+//}
+//int main() 
+//{
+//	int a = 1;
+//	int b = 4;
+//	int final = ADD(a, b);
+//	printf("%d", final);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//
+//void printMatrix(int array[][5],int row, int col)
+//{
+//	for (int i = 0; i < row; i++) 
+//	{
+//		for (int j = 0; j < col;j++) 
+//		{
+//			printf("%d ", array[i][j]);
+//		}
+//		printf("\n");
+//	}
+// 
+//}
+//int main()
+//{
+//	int matrix[3][5]={ 1,2,3,4,5,2,3,4,5,6,3,4,5,6,7 };
+//	int row = sizeof(matrix) / sizeof(matrix[0]);
+//	int col = sizeof(matrix) / sizeof(matrix[0][0])/row;
+//	//printf("%d %d", row, col);
+//
+//	printMatrix(matrix,row ,col );
+//
+//
+//	return 0;
+//}
+
+#include <stdio.h>
+int main() {
+	printf("%d", printf("%d", printf("%d", 43)));
+}
