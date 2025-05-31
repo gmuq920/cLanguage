95de0fd581a96c6638bf75413c93d8c587169964
gmuq920 <128571797+gmuq920@users.noreply.github.com>
2025-05-31 19:10:42 +0800

    Add files via upload

---
+++ Project_5_31/Project_5_31/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	printf("%s\n", __FILE__);
+//	printf("%d\n", __LINE__);
+//	printf("%s\n", __TIME__);
+//	printf("%s\n", __DATE__);
+//
+//	return 0;
+//}
+
+//
+//#define GENERIC_MAX(type)\
+//type type##_max(type x, type y)\
+//{\
+//     return x>y?x:y;\
+//}
+//
+//GENERIC_MAX(int)
+//GENERIC_MAX(float)
+//
+//
+//int main()
+//{
+//    int a = 10;
+//    int b = 20;
+//    int ret = int_max(a, b);
+//    printf("%d\n", ret);
+//    printf("%f\n", float_max(3.0, 8.0));
+//
+//    return 0;
+//}
